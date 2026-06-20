52cee0d325c5c569b29663204c723785e0384536
Prakash mandal <prakash909687@gmail.com>
2026-06-20 18:53:30 +0530

    Day   3:  Even odd  done   |   Phone se  | 22%  Loaded

---
+++ Day3_even_odd.py
@@ -0,0 +1,15 @@
+# Day 3 - DSA Question 3
+# Phone se commit kar raha hun. Laptop nehi hai.Par Himmat hai.
+numbers = [10,5,20,8,15,3,12,6]
+even_count = 0
+odd_count = 0
+for num in numbers:
+    if num % 2 == 0:
+       even_count = even_count + 1
+    else:
+      odd_count = odd_count + 1
+print(f"Total numbers: {len(numbers)}")
+print(f"Even numbers : {even_count}")
+print(f"odd numbers  : {odd_count}")
+print("50 LPA: 22% Loaded from Phone")
+print("Never Loved you ? Thanks for motivation 🙂")

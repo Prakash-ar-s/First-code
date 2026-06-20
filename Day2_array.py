d535f7b71e14080510989538673c22e10d061c32
Prakash mandal <prakash909687@gmail.com>
2026-06-20 13:34:17 +0530

    Day  2 : Array sum using for loop

--- Day2_array.py
+++ Day2_array.py
@@ -3,7 +3,7 @@
 
 numbers = [10,5,20,8,15]
 total = 0 # Sum store karne ke liye.Shuru mein 0
-for num in number: # Har number pe jao
+for num in numbers: # Har number pe jao
     total = total + num # total mein add karte jao
 print(f"Array ka total: {total}") # Answer 58
 print("50 LPA Journey: 21% Done")

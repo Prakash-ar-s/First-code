e01b3a8b3fa210d5242b2fde47a62e81985699c7
Prakash mandal <prakash909687@gmail.com>
2026-06-20 11:57:31 +0530

    Day  2 : Array sum using for loop
    Fix variable name in loop for summing array.

---
+++ Day2_array.py
@@ -0,0 +1,10 @@
+# Day 2 -  DSA Question 2
+# Array ka sum nikalna
+
+numbers = [10,5,20,8,15]
+total = 0 # Sum store karne ke liye.Shuru mein 0
+for num in number: # Har number pe jao
+    total = total + num # total mein add karte jao
+print(f"Array ka total: {total}") # Answer 58
+print("50 LPA Journey: 21% Done")
+print("Next: Question 3 karte hein")

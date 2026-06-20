ae915e59ecc49e35a54a2ab1bd1d0411cbf12d68
Prakash mandal <prakash909687@gmail.com>
2026-06-20 08:43:19 +0530

    Add initial DSA script for 50 LPA Journey

---
+++ Day1_dsa.py
@@ -0,0 +1,15 @@
+# 50 LPA Journey - Day 1
+Concept: if-else from Sololearn
+
+
+age = 26
+lpa_progress = 19
+if age >= 18:
+   print("Adult developer .Full price mehnat karni hai")
+else: 
+    print("Baccha hai abhi")
+if  lpa_progress >= 50:
+    print("Apply to companies")
+else:
+    print("DSA aur project banao")
+print(f"50 LPA Journey: {lpa_progress}% Done🚀")

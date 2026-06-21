3f017bc16bdffd6c72492574ca3b3984a1ad2725
Prakash mandal <prakash909687@gmail.com>
2026-06-21 19:14:15 +0530

    Day   7:  Fixed typos,   28% Loaded

--- day_7.py
+++ day_7.py
@@ -1,10 +1,10 @@
 # Lists Practice - 27% Loaded
 skills = ["Python", "GitHub", "English"]
 skills.append("Confidence")
-print(skklls) # Output: ['Python', 'GitHub', 'English', 'Confidence']
+print(skills) # Output: ['Python', 'GitHub', 'English', 'Confidence']
 lpa = 12
 for year in range (2026,2029):
    lpa = lpa * 2
    print(f"{year} mein LPA: {lpa}")
 # Output: 2026: 24,2027: 48,2028: 96
-print(50 LPA cross. Phone se kiya.")
+print("50 LPA cross. Phone se kiya.")
